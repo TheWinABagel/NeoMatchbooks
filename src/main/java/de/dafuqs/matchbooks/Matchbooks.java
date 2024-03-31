839cdc607dbf30387ce8a74f9929fd1d8131a08e
Ash (Bagel) <ash@rjohnston.net>
2024-03-31 15:04:49 -0500

    it actually works now

--- src/main/java/de/dafuqs/matchbooks/Matchbooks.java
+++ src/main/java/de/dafuqs/matchbooks/Matchbooks.java
@@ -3,18 +3,28 @@ package de.dafuqs.matchbooks;
 import com.mojang.logging.LogUtils;
 import de.dafuqs.matchbooks.recipe.MatchbookRecipeTypes;
 import de.dafuqs.matchbooks.recipe.matchbook.BuiltinMatchbooks;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-public class Matchbooks implements ModInitializer {
+@Mod(Matchbooks.MOD_ID)
+public class Matchbooks {
     public static final String MOD_ID = "matchbooks";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    @Override
-    public void onInitialize() {
+    public Matchbooks() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::onInitialize);
+        bus.addListener(MatchbookRecipeTypes::registerSerializer);
+    }
+
+
+    @SubscribeEvent
+    public void onInitialize(FMLCommonSetupEvent event) {
         BuiltinMatchbooks.init();
-        MatchbookRecipeTypes.init();
         LOGGER.info("Initialized Matchbooks");
     }
 
--- src/main/java/de/dafuqs/matchbooks/recipe/MatchbookRecipeTypes.java
+++ src/main/java/de/dafuqs/matchbooks/recipe/MatchbookRecipeTypes.java
@@ -1,12 +1,15 @@
 package de.dafuqs.matchbooks.recipe;
 
 import de.dafuqs.matchbooks.Matchbooks;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 
 public class MatchbookRecipeTypes {
 
-    public static void init() {
-        Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, Matchbooks.id("item_damaging"), ItemDamagingRecipe.Serializer.INSTANCE);
+    @SubscribeEvent
+    public static void registerSerializer(RegisterEvent e) {
+        e.register(ForgeRegistries.RECIPE_SERIALIZERS.getRegistryKey(),
+                helper -> helper.register(Matchbooks.id("item_damaging"), ItemDamagingRecipe.Serializer.INSTANCE));
     }
 }
